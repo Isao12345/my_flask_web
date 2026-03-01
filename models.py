e75786c85e06d21ec675b201362a4a87d4959c6e
Saswarn Binsa-i <6810110066@psu.ac.th>
2026-03-01 16:35:31 +0700

    improve user password security and transaction date type

--- models.py
+++ models.py
@@ -1,4 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
+from datetime import datetime
 
 db = SQLAlchemy()
 
@@ -6,7 +8,13 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), unique=True, nullable=False)
     email = db.Column(db.String(100), unique=True, nullable=False)
-    password = db.Column(db.String(200), nullable=False)
+    password_hash = db.Column(db.String(200), nullable=False)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
 
 class Category(db.Model):
@@ -20,7 +28,7 @@ class Transaction(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     amount = db.Column(db.Float, nullable=False)
     note = db.Column(db.String(200))
-    date = db.Column(db.String(20))
+    date = db.Column(db.DateTime, default=datetime.utcnow)
     type = db.Column(db.String(10))  # income / expense
 
     user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
