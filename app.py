103401c9cb35e071b90052368f8376cf2286eff0
Saswarn Binsa-i <6810110066@psu.ac.th>
2026-03-01 16:21:49 +0700

    setup sqlalchemy configuration

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask, render_template 
+from flask_sqlalchemy import SQLAlchemy   
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "devkey"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///expense.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
 
 
 @app.route("/")
