d48fd0692e25211ce8712db83a1aa81956bc3996
Saswarn Binsa-i <6810110066@psu.ac.th>
2026-03-01 16:18:09 +0700

    add register route

--- app.py
+++ app.py
@@ -33,5 +33,10 @@ def categories():
 def login():
     return render_template("login.html")
 
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    return render_template("register.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
