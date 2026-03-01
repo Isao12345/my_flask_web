56fe9a6626cc3384b238ac6f726b6d3ab737dbe0
Saswarn Binsa-i <6810110066@psu.ac.th>
2026-03-01 16:23:28 +0700

    create category model

--- models.py
+++ models.py
@@ -7,3 +7,10 @@ class User(db.Model):
     username = db.Column(db.String(50), unique=True, nullable=False)
     email = db.Column(db.String(100), unique=True, nullable=False)
     password = db.Column(db.String(200), nullable=False)
+
+
+class Category(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    type = db.Column(db.String(10), nullable=False)  # income / expense
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
