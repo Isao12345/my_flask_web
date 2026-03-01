8558922c7130ac542c6c47c28a70fc33311a4413
Saswarn Binsa-i <6810110066@psu.ac.th>
2026-03-01 16:03:17 +0700

    add expenses route

--- app.py
+++ app.py
@@ -3,9 +3,21 @@ from flask import Flask, render_template
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "devkey"
 
+
 @app.route("/")
 def dashboard():
     return render_template("dashboard.html")
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/expenses")
+def expenses():
+    return render_template("expenses.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
